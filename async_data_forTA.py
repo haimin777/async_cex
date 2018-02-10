5470246297242ba08127d55ebfbd77f71be07ae7
haimin777 <haimin777@yandex.ru>
2018-02-10 21:57:01 +0300

    Asyncio get ohlc data added

--- async_data_forTA.py
+++ async_data_forTA.py
@@ -1,42 +1,32 @@
-# cryptocurrency data for TA in form of dataframe
-
-import ccxt.async as ccxt
-import datetime
-import time
-import pandas as pd
-import  numpy as np
-import talib
 import asyncio
-import os
-import sys
-
-class DataForTA:
-    def __init__(self):
-        self.data = []
-        self.hold = 30
-
-    def get_dataframe(self,pair, lim):
-        try:
-
-            ohlcvs = asyncio.get_event_loop().run_until_complete(ccxt.cex().fetch_ohlcv(pair, '1m', limit=lim))
-
-           # ohlcvs = self.exchange.fetch_ohlcv('ETH/BTC', '5m', limit=lim)  # , from_timestamp, 100)
-           # print(self.exchange.timeframes) #вывод доступных таймфреймов для данного рынка
+import ccxt.async as ccxt
+import pandas as pd
 
 
-            df = pd.DataFrame(np.array(ohlcvs), columns=('time', 'open',
-                                                         'hight', 'low',
-                                                         'close', 'volume'))
+async def ohlcv_cex(pair):
+    cex = ccxt.cex()
+    while True:
+        await asyncio.sleep(5)
+        data = await cex.fetch_ohlcv(pair, '1m', limit=10)
+        print('ohlc fetched', pair)
+        df = pd.DataFrame(data, columns=('time', 'open',
+                                         'hight', 'low',
+                                         'close', 'volume'))
+        print(df)
 
-            print('data loaded OK')
-            return df
+    return df
 
-        except (ccxt.ExchangeError, ccxt.AuthenticationError, ccxt.ExchangeNotAvailable, ccxt.RequestTimeout) as error:
-            print('Got an error', type(error).__name__, error.args, ', retrying in', self.hold, 'seconds...')
-            time.sleep(self.hold)
 
-if __name__ == "__main__":
-    d = DataForTA()
-    f = d.get_dataframe(10)
+loop = asyncio.get_event_loop()
 
-    print(talib.SMA(f.close.values, 5)[-1])
+try:
+    pair = ['ETH/EUR', 'BTC/USD', 'BTC/GBP']
+    task_list = [loop.create_task(ohlcv_cex(pair[i])) for i in range(3)]
+    asyncio.wait(task_list)
+    loop.run_forever()
+
+except KeyboardInterrupt:
+    pass
+finally:
+    print("Closing Loop")
+    loop.close()
